8e86526fd663f531c8006d3f061fba2a39660596
nilu986 <nilu.986@gmail.com>
2023-08-18 13:27:44 +0530

    Mock_round_1st

---
+++ test.js
@@ -0,0 +1,23 @@
+var myObject = {
+  foo: "bar",
+  lname: 'test',
+  
+};// 
+var newObj = Object.assign({}, myObject);// {...myObject}
+
+Program To Check For Balanced Brackets In An Expression
+Input: exp = “[()]{}{[()()]()}” 
+Output: Balanced
+Input: exp = “[(])” 
+Output: Not Balanced 
+
+Given the following array: [2,3,4,5,7]
+Perform 3 right rotations:
+First rotation : [7,2,3,4,5] , Second rotation : [5,7,2,3,4] and, Third rotation: [4,5,7,2,3]
+return [4,5,7,2,3]
+
+Write a function that takes an array as input and returns a new array with duplicate elements removed. For example, if the input is [1, 2, 3, 2, 4, 1], the output should be [1, 2, 3, 4].
+
+from Lakshya Arora (internal) to everyone:    5:30 PM
+Write a function that takes a nested array as input and returns a flattened array. For example, if the input is [1, [2, [3, 4], 5]], the output should be [1, 2, 3, 4, 5].
+
